ee8aabfad040b58443baf0b3cc77e60c5907b438
muneet <muneetchaudhary@me.com>
2023-02-10 17:21:35 +0100

    initial commit

---
+++ src/Controller/ProjectController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Project;
+
+#[Route('/api', name: 'app_')]
+
+class ProjectController extends AbstractController
+{
+    #[Route('/article', name: 'article_index', methods: ['GET'])]
+    public function index(ManagerRegistry $doctrine): Response
+    {
+        $products = $doctrine
+            ->getRepository(Project::class)
+            ->findAll();
+
+        $data = [];
+
+        foreach ($products as $product) {
+            $data[] = [
+                'id' => $product->getId(),
+                'name' => $product->getName(),
+                'description' => $product->getDescription(),
+                'price' => $product->getPrice(),
+            ];
+        }
+        return $this->json($data);
+    }
+
+    #[Route('/article', name: 'article_create', methods: ['POST'])]
+    public function new(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $entityManager = $doctrine->getManager();
+
+        $project = new Project();
+        $project->setName($request->request->get('name'));
+        $project->setDescription($request->request->get('description'));
+        $project->setPrice($request->request->get('price'));
+
+        $entityManager->persist($project);
+        $entityManager->flush();
+
+        return $this->json('Created new article successfully with id ' . $project->getId());
+    }
+
+    #[Route('/article/{id}', name: 'article_show', methods: ['GET'])]
+    public function show(ManagerRegistry $doctrine, int $id): Response
+    {
+        $project = $doctrine->getRepository(Project::class)->find($id);
+
+        if (!$project) {
+            return $this->json('No project found for id' . $id, 404);
+        }
+
+        $data =  [
+            'id' => $project->getId(),
+            'name' => $project->getName(),
+            'description' => $project->getDescription(),
+            'price' => $project->getPrice(),
+        ];
+
+        return $this->json($data);
+    }
+
+    #[Route('/article/{id}', name: 'article_edit', methods: ['PUT'])]
+    public function edit(ManagerRegistry $doctrine, Request $request, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $project = $entityManager->getRepository(Project::class)->find($id);
+
+        if (!$project) {
+            return $this->json('No article found for id' . $id, 404);
+        }
+
+        $project->setName($request->request->get('name'));
+        $project->setDescription($request->request->get('description'));
+        $project->setPrice($request->request->get('price'));
+        $entityManager->flush();
+
+        $data =  [
+            'id' => $project->getId(),
+            'name' => $project->getName(),
+            'description' => $project->getDescription(),
+            'price' => $project->getPrice(),
+        ];
+
+        return $this->json($data);
+    }
+
+    #[Route('/article/{id}', name: 'article_delete', methods: ['DELETE'])]
+    public function delete(ManagerRegistry $doctrine, int $id): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $project = $entityManager->getRepository(Project::class)->find($id);
+
+        if (!$project) {
+            return $this->json('No article found for id' . $id, 404);
+        }
+
+        $entityManager->remove($project);
+        $entityManager->flush();
+
+        return $this->json('Deleted article successfully');
+    }
+}
